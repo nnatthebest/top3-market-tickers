d378526087f251f3b2e53a917ebf94bfc7f827d2
Z Di <mymewmail@gmail.com>
2017-04-28 01:46:12 +0300

    Выделилв отдельный модуль Tickers

--- src/index.js
+++ src/index.js
@@ -1,72 +1,4 @@
-var request = require('request');
-var models = require('./server/models/index');
-
-
-
-class Tickers {
-	
-	constructor(urlInfo = '', urlApi = '',	tickerBuy = 'buy',
-				tickerSell = 'sell', marketName = ''){
-			
-		this.urlInfo = urlInfo;
-		this.urlApi = urlApi;
-		this.tickerBuy = tickerBuy;
-		this.tickerSell = tickerSell;
-		this.marketName = marketName;
-		this.lines = [];
-	}
-	
-	
-	getAllExistTickers (callback) {			
-		request(this.urlInfo, (error, response, body) => {
-			if (!error && response.statusCode == 200) {
-				let data = JSON.parse(body);
-				let keys = Object.keys(data.pairs);
-				let apiUrl = keys.join('-');
-				//Сформированная строка запроса
-				this.urlApi = this.urlApi + apiUrl+'?ignore_invalid=1';
-			}
-			callback('next');
-		});
-	}
-
-	getTickersValue(callback) { 
-	var self = this;
-		request(this.urlApi, function (error, response, body) {
-			let lines = [];
-			if (!error && response.statusCode == 200) {
-				let data = JSON.parse(body);
-				const keys = Object.keys(data);
-				let dt = new Date()
-				const createdTime = (dt.getTime()/1000);
-				for (let k of keys) {
-					let obj = data[k];
-					lines.push({'marketName':self.marketName, 
-								'tickerPair':k.toLowerCase(), 
-								'tickerBuy':obj[self.tickerBuy], 
-								'tickerSell':obj[self.tickerSell], 
-								'createdTime':createdTime});
-				}		
-			}
-			callback(lines);
-		});
-	}
-
-	writeToDatabase(lines){
-		lines ? '': console.log('Get value is\'t empty');
-		for (let line of lines) {
-			models.Tickers.create({
-				marketName: line.marketName,
-				tickerPair: line.tickerPair,
-				tickerBuy: line.tickerBuy,
-				tickerSell: line.tickerSell,
-				createdTime: line.createdTime  
-			});		
-		}
-	}
-}
-
-
+var Tickers = require('./tickers.js');
 
 class marketExmo extends Tickers {
 	constructor() {				
@@ -79,7 +11,8 @@ class marketExmo extends Tickers {
 	
 	make (){
 		this.getTickersValue((lines)=>{
-				this.writeToDatabase(lines)		
+				//this.writeToDatabase(lines)	
+				console.log('exmo');	
 			});		
 		}
 	}
@@ -96,7 +29,8 @@ class marketPolonex extends Tickers {
 	
 	make (){
 		this.getTickersValue((lines)=>{
-				this.writeToDatabase(lines)		
+				//this.writeToDatabase(lines)	
+				console.log('poloniex');	
 			});		
 		}
 	}
@@ -115,22 +49,25 @@ class marketBtcE extends Tickers {
 	make (){
 		this.getAllExistTickers(()=>{
 			this.getTickersValue((lines)=>{
-				this.writeToDatabase(lines);		
+				//this.writeToDatabase(lines);
+				console.log('btc-e');
 				});
 			});
 		
 		}
 	}
 
-setInterval(begineInterval,30000);
+setInterval(begineInterval,3000);
 
 function begineInterval() {
-	return Promise.resolve().then(()=>{
+	return Promise.resolve().then( () => {
 		let btc_e = new marketBtcE();
-		let poloniex = new marketPolonex();
-		let exmo = new marketExmo();
 		btc_e.make();
+		
+		let poloniex = new marketPolonex();
 		poloniex.make();
+		
+		let exmo = new marketExmo();
 		exmo.make();
 		});
 };
